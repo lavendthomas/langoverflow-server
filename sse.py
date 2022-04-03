d4309fd9489e3d1e5b600625ea0ada267b0fd386
Thomas Lavend'Homme <lavendthomas@outlook.be>
2022-04-03 11:12:06 +0200

    Change /user endpoint to GET and POST

--- sse.py
+++ sse.py
@@ -64,7 +64,7 @@ def login():
     db.session.commit()
     return user.to_json()
 
-@app.route('/user', methods=['GET'])
+@app.route('/user', methods=['GET', "POST"])
 @cross_origin()
 def user():
     data = json.loads(request.data)
